eeeec205d730c6f23a437dff5109d9b7874e6f06
mohincse <mohin@appscode.com>
2020-01-20 18:15:44 +0600

    add vue-tooltip

--- src/components/ComponentErrors.vue
+++ src/components/ComponentErrors.vue
@@ -1,5 +1,5 @@
 <template>
-  <span v-show="errors.length > 0" class="is-warning" :title="errors[0]">
+  <span v-show="errors.length > 0" class="is-warning" v-tooltip="errors[0]">
    <i class="fa fa-warning"></i>
     error in {{ errors.length }} filds
   </span>
