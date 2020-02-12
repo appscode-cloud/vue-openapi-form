7a557e2227d6df585e5e01bb08e7f42244ac315a
M. A. Muhaimin Sakib <sakib@appscode.com>
2020-02-12 11:29:41 +0600

    Add method to clean object and arrays and prevent number being converted to string
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

--- src/mixins/model.js
+++ src/mixins/model.js
@@ -33,7 +33,47 @@ export const model = {
       if (this.type === "object" || this.type === "key-value-pairs") return {};
       else if (this.type === "array") return [];
       else if (this.type === "boolean") return false;
+      else if (this.type === "number") return null;
       else return "";
+    },
+    clean(ob) {
+      if (this.type === "object" || this.type === "key-value-pairs") {
+        Object.keys(ob).forEach(key => {
+          let stringify = "";
+          if (typeof ob[key] !== "string") stringify = JSON.stringify(ob[key]);
+          else stringify = ob[key];
+          if (
+            stringify === undefined ||
+            stringify === "null" ||
+            stringify === "" ||
+            stringify === "{}" ||
+            stringify === "[]"
+          ) {
+            delete ob[key];
+          }
+        });
+      } else if (this.type === "array") {
+        let arrayOfDeleteIndexes = ob
+          .map((item, idx) => ({ item, idx }))
+          .filter(el => {
+            const item = el.item;
+            let stringify = "";
+            if (typeof item !== "string") stringify = JSON.stringify(item);
+            else stringify = item;
+
+            if (
+              stringify === undefined ||
+              stringify === "null" ||
+              stringify === "" ||
+              stringify === "{}" ||
+              stringify === "[]"
+            ) {
+              return true;
+            } else return false;
+          })
+          .map(item => item.idx);
+        arrayOfDeleteIndexes.forEach(idx => ob.splice(idx, 1));
+      }
     }
   },
 
@@ -47,7 +87,15 @@ export const model = {
       deep: true,
       handler(newVal, oldVal) {
         if (oldVal !== null && oldVal !== undefined) {
-          this.$emit("input", newVal);
+          // clean the newVal if it's array or object
+          this.clean(newVal);
+
+          // prevent number from converting to string
+          if (this.type === "number") {
+            // if the newVal string is empty, emit null
+            if (newVal === "") this.$emit("input", null);
+            else this.$emit("input", +newVal);
+          } else this.$emit("input", newVal);
         }
       }
     }
