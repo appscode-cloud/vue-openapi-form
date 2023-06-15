6c151345f1fe71ee3544696c24a3739ffaeaaa26
Nihal-Azmain <nihal@appscode.com>
2023-06-15 14:30:10 +0600

    updated class in VueOpenApiForm.vue
    Signed-off-by: Nihal-Azmain <nihal@appscode.com>

--- src/components/VueOpenapiForm.vue
+++ src/components/VueOpenapiForm.vue
@@ -1,7 +1,7 @@
 <template>
   <v-form ref="v-form" v-slot="{ meta, validate, errors }" as="">
     <ac-form>
-      <div class="vue-openapi-form" :class="{ 'is-medium': size === 'medium' }">
+      <div class="vue-openapi-form pl-20" :class="{ 'is-medium': size === 'medium' }">
         <v-field
           v-slot="{ field, handleChange }"
           v-model="modelData"
