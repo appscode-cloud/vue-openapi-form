f5336e7710eee9719e9c78dcecd432914fb1fb7e
mohin7 <mohin@appscode.com>
2023-10-09 14:27:20 +0600

    Update design-system version, error color
    Signed-off-by: mohin7 <mohin@appscode.com>

--- src/components/SimpleInput.vue
+++ src/components/SimpleInput.vue
@@ -100,9 +100,24 @@
               validationOb.errors &&
               validationOb.errors.length > 0
             "
-            class="is-error"
+            class="is-flex gap-4 mt-4 has-text-danger"
           >
-            <span class="warning"><i class="fa fa-warning"></i></span>
+            <span class="icon"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            </span>
             {{ validationOb.errors[0] }}
           </p>
         </div>
