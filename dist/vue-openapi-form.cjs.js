12c8b6822f31f7fafbe18da00e11568d397208c9
mohin7 <mohin@appscode.com>
2023-10-18 15:18:56 +0600

    Generate pkg
    Signed-off-by: mohin7 <mohin@appscode.com>

--- dist/vue-openapi-form.cjs.js
+++ dist/vue-openapi-form.cjs.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("./entry-ed862885.js");require("vue"),exports.VueOpenapiForm=e.s,exports.default=e.p;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("./entry-b87462df.js");require("vue"),exports.VueOpenapiForm=e.s,exports.default=e.p;
--- dist/vue-openapi-form.esm.js
+++ dist/vue-openapi-form.esm.js
@@ -1,1 +1,1 @@
-export{s as VueOpenapiForm,p as default}from"./entry-42510bea.js";import"vue";
+export{s as VueOpenapiForm,p as default}from"./entry-55c11dc9.js";import"vue";
