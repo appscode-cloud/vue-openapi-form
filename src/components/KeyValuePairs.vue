f3c6e59a3f2e3d95be93edf441d1cd03d6fcf659
SayedTahsin <mail.tahsin99@gmail.com>
2024-06-12 12:58:00 +0600

    converted to script-setup for keyValuePairs comp
    Signed-off-by: SayedTahsin <mail.tahsin99@gmail.com>

--- src/components/KeyValuePairs.vue
+++ src/components/KeyValuePairs.vue
@@ -194,154 +194,294 @@
   </div>
 </template>
 
-<script>
-import { model } from '../mixins/model.js';
-import tabs from '../mixins/tabs.js';
-import validation from '../mixins/validation.js';
-import size from '../mixins/size.js';
-import { defineAsyncComponent, defineComponent } from 'vue';
+<script setup>
+import { defineAsyncComponent, onMounted, watch, ref, computed } from 'vue';
 
-export default defineComponent({
-  name: 'KeyValuePairs',
+const Tabs = defineAsyncComponent(() =>
+  import('./Tabs.vue').then((module) => module.default)
+);
+const JsonForm = defineAsyncComponent(() =>
+  import('./JsonForm.vue').then((module) => module.default)
+);
+const YamlForm = defineAsyncComponent(() =>
+  import('./YamlForm.vue').then((module) => module.default)
+);
+const VForm = defineAsyncComponent(() =>
+  import('vee-validate').then(({ Form }) => Form)
+);
+const VField = defineAsyncComponent(() =>
+  import('vee-validate').then(({ Field }) => Field)
+);
+const ComponentErrors = defineAsyncComponent(() =>
+  import('./ComponentErrors.vue').then((module) => module.default)
+);
+const KeyValuePairItems = defineAsyncComponent(() =>
+  import('./sub-components/KeyValuePairItems.vue').then(
+    (module) => module.default
+  )
+);
+const ObjectFormWrapper = defineAsyncComponent(() =>
+  import('@/components/ObjectFormWrapper.vue').then((module) => module.default)
+);
+const ArrayInput = defineAsyncComponent(() =>
+  import('@/components/ArrayInput.vue').then((module) => module.default)
+);
+const KeyValuePairs = defineAsyncComponent(() =>
+  import('@/components/KeyValuePairs.vue').then((module) => module.default)
+);
+const SimpleInput = defineAsyncComponent(() =>
+  import('@/components/SimpleInput.vue').then((module) => module.default)
+);
 
-  components: {
-    KeyValuePairItems: defineAsyncComponent(() =>
-      import('./sub-components/KeyValuePairItems.vue').then(
-        (module) => module.default
-      )
-    ),
+const props = defineProps({
+  isSelfRequired: {
+    type: Boolean,
+    default: false,
   },
-
-  mixins: [model, tabs, validation, size],
-  props: {
-    schema: {
-      type: Object,
-      default: () => ({}),
-    },
-    fieldName: {
-      type: String,
-      default: '',
-    },
-    modelValue: {
-      type: Object,
-      default: () => ({}),
-    },
-    errors: {
-      type: Object,
-      default: () => ({}),
-    },
-    isLastChild: {
-      type: Boolean,
-      default: false,
-    },
+  type: {
+    type: String,
+    default: 'string',
   },
-
-  data() {
-    return {
-      newData: null,
-      updatePass: 0,
-      keyValueArray: null,
-      referencekeyValueArray: null,
-      newKey: '',
-      newValue: null,
-    };
+  referenceModel: {
+    type: null,
+    default: () => ({}),
   },
-
-  computed: {
-    additionalProperties() {
-      return this.schema.additionalProperties || {};
-    },
+  schema: {
+    type: Object,
+    default: () => ({}),
   },
-
-  watch: {
-    keyValueArray: {
-      immediate: true,
-      deep: true,
-      handler(newVal, oldVal) {
-        if (oldVal !== null && oldVal !== undefined) {
-          const stringNewVal = JSON.stringify(newVal);
-          this.modelData = this.reconstructObject(JSON.parse(stringNewVal));
-        }
-      },
-    },
-
-    activeTab() {
-      // re-calculate keyValueArray
-      this.initKeyValueArray();
-      this.initReferenceKeyValueArray();
-    },
-
-    modelValue: {
-      deep: true,
-      immediate: true,
-      handler(n, o) {
-        const newStringifiedObject = JSON.stringify(n);
-        const oldStringifiedObject = JSON.stringify(o);
-        if (newStringifiedObject !== oldStringifiedObject)
-          this.initKeyValueArray();
-
-        this.initReferenceKeyValueArray();
-      },
-    },
+  fieldName: {
+    type: String,
+    default: '',
   },
-
-  methods: {
-    initKeyValueArray() {
-      this.keyValueArray = Object.keys(this.modelValue).map((key) => ({
-        key,
-        value: this.modelValue[key] || null,
-      }));
-    },
-    initReferenceKeyValueArray() {
-      this.referencekeyValueArray = Object.keys(this.referenceModel).map(
-        (key) => ({
-          key,
-          value: this.referenceModel[key] || null,
-        })
-      );
-    },
-
-    updateKeyValueArray(value) {
-      this.keyValueArray = Object.keys(value).map((key) => ({
-        key,
-        value: value[key],
-      }));
-    },
-
-    reconstructObject(arr) {
-      let result = {};
-      arr.forEach((item) => {
-        result = Object.assign(
-          {},
-          { ...result },
-          {
-            [`${item.key}`]: item.value,
-          }
-        );
-      });
-      return result;
-    },
-
-    async addProp(validate) {
-      const { valid } = await validate();
-
-      if (valid) {
-        this.keyValueArray.push({
-          key: this.newKey,
-          value: this.newValue,
-        });
-
-        this.newKey = '';
-        this.newValue = null;
-
-        this.updatePass += 1;
-      }
-    },
-
-    deleteProp(index) {
-      this.keyValueArray.splice(index, 1);
-      this.updatePass += 1;
-    },
+  modelValue: {
+    type: Object,
+    default: () => ({}),
+  },
+  errors: {
+    type: Object,
+    default: () => ({}),
+  },
+  isLastChild: {
+    type: Boolean,
+    default: false,
   },
 });
+const emit = defineEmits(['update:modelValue']);
+
+const activeTab = ref('form');
+const isMedium = ref(false);
+const updatePass = ref(0);
+const keyValueArray = ref(null);
+const referencekeyValueArray = ref(null);
+const newKey = ref('');
+const modelData = ref(null);
+const newValue = ref(null);
+
+const additionalProperties = computed(() => {
+  return props.schema.additionalProperties || {};
+});
+
+watch(
+  modelData,
+  (newVal, oldVal) => {
+    if (oldVal !== null && oldVal !== undefined) {
+      // clean the newVal if it's array or object if the cleanObject global data is true
+      // if (this.cleanObject) this.clean(newVal);
+
+      // prevent number from converting to string
+      if (props.type === 'number' || props.type === 'integer') {
+        // if the newVal string is empty, emit null
+        if (newVal === '') emit('update:modelValue', null);
+        else emit('update:modelValue', +newVal);
+      } else emit('update:modelValue', newVal);
+    }
+  },
+  { immediate: true, deep: true }
+);
+
+watch(
+  () => props.modelValue,
+  (newVal, oldVal) => {
+    // do this only once, when the value object is initialized after api call or some delay
+    if (JSON.stringify(oldVal) !== JSON.stringify(newVal)) initModelData();
+  },
+  { deep: true }
+);
+
+watch(
+  keyValueArray,
+  (newVal, oldVal) => {
+    if (oldVal !== null && oldVal !== undefined) {
+      const stringNewVal = JSON.stringify(newVal);
+      modelData.value = reconstructObject(JSON.parse(stringNewVal));
+    }
+  },
+  { immediate: true, deep: true }
+);
+
+watch(activeTab, () => {
+  // re-calculate keyValueArray
+  initKeyValueArray();
+  initReferenceKeyValueArray();
+});
+
+watch(
+  () => props.modelValue,
+  (n, o) => {
+    const newStringifiedObject = JSON.stringify(n);
+    const oldStringifiedObject = JSON.stringify(o);
+    if (newStringifiedObject !== oldStringifiedObject) initKeyValueArray();
+
+    initReferenceKeyValueArray();
+  }
+);
+
+onMounted(() => {
+  initModelData();
+  const form = document.querySelector('.vue-openapi-form');
+  if (form.classList.contains('is-medium')) {
+    isMedium.value = true;
+  }
+});
+
+const calcFormErrors = (errors, prefix) => {
+  return Object.keys(errors)
+    .filter((key) => key.startsWith(prefix))
+    .map((key) => {
+      const path = key.replace(/^(\$\/)/, '');
+      const prefixedPath = prefix.replace(/^(\$\/)/, '');
+
+      const relativePath = path.replace(prefixedPath + '/', '');
+
+      const pfx = relativePath.includes('/') ? `${relativePath}: ` : '';
+      return `${pfx}${errors[key]}`;
+    });
+};
+const initModelData = () => {
+  if (props.modelValue) {
+    if (
+      (props.type === 'object' || props.type === 'key-value-pairs') &&
+      Object.keys(props.modelValue).length > 0
+    )
+      modelData.value = JSON.parse(JSON.stringify(props.modelValue));
+    else if (props.type === 'array' && props.modelValue.length > 0)
+      modelData.value = JSON.parse(JSON.stringify(props.modelValue));
+    else if (props.type === 'boolean' && props.modelValue !== null)
+      modelData.value = props.modelValue;
+    else if (
+      props.type === 'string' ||
+      props.type === 'number' ||
+      props.type === 'integer'
+    )
+      modelData.value = props.modelValue;
+    else modelData.value = initWithBlank();
+  } else modelData.value = initWithBlank();
+};
+
+const initWithBlank = () => {
+  if (props.type === 'object' || props.type === 'key-value-pairs') return {};
+  else if (props.type === 'array') return [];
+  else if (props.type === 'boolean') return false;
+  else if (props.type === 'number' || props.type === 'integer') return null;
+  else return '';
+};
+
+const clean = (ob) => {
+  if (props.type === 'object' || props.type === 'key-value-pairs') {
+    Object.keys(ob).forEach((key) => {
+      let stringify = '';
+      if (typeof ob[key] !== 'string') stringify = JSON.stringify(ob[key]);
+      else stringify = ob[key];
+      if (
+        stringify === undefined ||
+        stringify === 'null' ||
+        stringify === '' ||
+        stringify === '{}' ||
+        stringify === '[]'
+      ) {
+        delete ob[key];
+      }
+    });
+  } else if (props.type === 'array') {
+    let arrayOfDeleteIndexes = ob
+      .map((item, idx) => ({ item, idx }))
+      .filter((el) => {
+        const item = el.item;
+        let stringify = '';
+        if (typeof item !== 'string') stringify = JSON.stringify(item);
+        else stringify = item;
+
+        if (
+          stringify === undefined ||
+          stringify === 'null' ||
+          stringify === '' ||
+          stringify === '{}' ||
+          stringify === '[]'
+        ) {
+          return true;
+        } else return false;
+      })
+      .map((item) => item.idx);
+    arrayOfDeleteIndexes.forEach((idx) => ob.splice(idx, 1));
+  }
+};
+
+const initKeyValueArray = () => {
+  keyValueArray.value = Object.keys(props.modelValue).map((key) => ({
+    key,
+    value: props.modelValue[key] || null,
+  }));
+};
+
+const initReferenceKeyValueArray = () => {
+  referencekeyValueArray.value = Object.keys(props.referenceModel).map(
+    (key) => ({
+      key,
+      value: props.referenceModel[key] || null,
+    })
+  );
+};
+
+const updateKeyValueArray = (value) => {
+  keyValueArray.value = Object.keys(value).map((key) => ({
+    key,
+    value: value[key],
+  }));
+};
+
+const reconstructObject = (arr) => {
+  let result = {};
+  arr.forEach((item) => {
+    result = Object.assign(
+      {},
+      { ...result },
+      {
+        [`${item.key}`]: item.value,
+      }
+    );
+  });
+  return result;
+};
+
+const addProp = async (validate) => {
+  const { valid } = await validate();
+
+  if (valid) {
+    keyValueArray.value.push({
+      key: newKey.value,
+      value: newValue.value,
+    });
+
+    newKey.value = '';
+    newValue.value = null;
+
+    updatePass.value += 1;
+  }
+};
+
+const deleteProp = (index) => {
+  keyValueArray.value.splice(index, 1);
+  updatePass.value += 1;
+};
 </script>
